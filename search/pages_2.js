b70970de5f0ffb94434184ff0e48c0caf7614f1b
albert-github <albert-github@users.noreply.github.com>
2023-03-29 15:06:59 +0000

    deploy: c43681a928f2fa76c6334af658ccb536fb093fb6

--- search/pages_2.js
+++ search/pages_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['todo_20list_0',['Todo List',['../dd/da0/todo.html',1,'']]],
-  ['translator_20report_1',['Translator report',['../d2/d78/pg_trans.html',1,'']]]
+  ['todo_20list_0',['Todo List',['../dd/da0/todo.html',1,'']]]
 ];
